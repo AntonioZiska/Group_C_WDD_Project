0bd2dcef264bf389310fc4576c638046c7616f96
AntonioZiska <antonioziska@gmail.com>
2024-12-09 13:39:52 +0000

    final changes

--- temp_test/js/external.js
+++ temp_test/js/external.js
@@ -1,54 +1,12 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/*Antonio's code*/
+/* When the user goes over the button/leaves the dropdown content menu toggle between hiding and showing the dropdown content */
 function drpdwnw() {
-  document.getElementById("drpdwncw").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+	document.getElementById("drpdwncw").classList.toggle("show");
 }
 function drpdwnp() {
-  document.getElementById("drpdwncp").classList.toggle("show");
+	document.getElementById("drpdwncp").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
-
-
-
-/*function random() {
-	
-	let r = Math.floor(Math.random()*10);
-	let page = document.getElementById("demo");
-	page.innerHTML=r;
-	page.style.backgroundColor="olive";
-	page.style.color = "white";
-	
-}
-*/
 
 
 
@@ -97,3 +55,60 @@ function validate() {
 	}
 	return valid;
 }
+
+/*additional info peace js*/
+let txtn = 0;
+
+function nexttxt() {
+	/* Array with the different text options for both header and txt all in one*/
+	let randtext = [
+		"Long-Term Sustainability", 
+		"Living in peace allows us to focus on long-term planning and ensuring a sustainable future. As well as the development of necessary infrastructure, protection of the environment, and economic planning that meets the current and future needs of the world.",
+		"World wide Peace", 
+		"Peace across the world, allows nations to collaborate on issues like climate change, human rights, and global health. By avoiding conflict we are more likely to work together for the common good of humanity. Peace throughout the world would also mean an end to the senseless loss of human life in war.",
+		"Earth's recovery", 
+		"We tend to value human life above all others, but it's not the only casualty of constant war. That is to say, peace would bring about a time of recovery for the Earth itself. Not to mention that it would allow the many living creatures apart from humans a chance to recover from the carnage wrought by war."
+	];
+
+	/*header*/
+	let headerText = randtext[txtn];
+	
+	document.getElementById("txh").innerText = headerText;
+
+	/*paragraph*/
+	let paragraphText = randtext[txtn + 1];
+
+	document.getElementById("txt").innerText = paragraphText;
+
+	/* increment*/
+	txtn += 2;
+
+	/*reset*/
+	if (txtn >= randtext.length) {
+		txtn = 0;
+	}
+}
+
+
+let cimg = 0;
+
+function nextimg() {
+	let array = ["img/gandhi1.png", "img/gandhi2.png", "img/mandela1.jpg", "img/mandela2.jpg"];
+	
+	let doc = document.getElementById("img");
+	
+	/* Set the src to be that of the current array element*/
+	doc.setAttribute("src", array[cimg]);
+	
+	/*increment*/
+	cimg++;
+	
+	/*reset*/
+	if (cimg >= array.length) {
+		cimg = 0;
+	}
+}
+
+
+
+
